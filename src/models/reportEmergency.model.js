935d067a673f12d6fcf2d5802c881d8b38e2d0cf
Prashant Rajawat <prashantsinghrajawat27@gmail.com>
2025-02-11 22:42:24 +0530

    report emergency schema added

--- src/models/reportEmergency.model.js
+++ src/models/reportEmergency.model.js
@@ -1,5 +1,42 @@
 import mongoose from "mongoose";
 
-const reportEmergencySchema = new mongoose.Schema({});
+const reportEmergencySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  mobileNumber: { type: String, required: true },
+  selectedDoctors: [
+    {
+      name: String,
+      phone: String,
+      clinic: String,
+      address: String,
+      distance: String,
+      donationRequired: Boolean,
+    },
+  ],
+  emergencyType: String,
+  uploadedFile: String,
+  recordedAudio: String,
+  animalType: {
+    type: String,
+    enum: [
+      "Dog",
+      "Cat",
+      "Other Large Animal",
+      "Cow",
+      "Goat/Sheep",
+      "Other Small Animal",
+    ],
+  },
+  location: {
+    liveLocation: Boolean,
+    currentLocation: Boolean,
+  },
+  requestVideoCall: Boolean,
+  makeDonation: Boolean,
+  createdAt: { type: Date, default: Date.now },
+});
 
-export const reportEmergency = mongoose.Model("report", reportEmergencySchema);
+export const ReportEmergency = mongoose.model(
+  "ReportEmergency",
+  reportEmergencySchema
+);
